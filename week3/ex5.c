725a1985cdb6f09f0d64f899a73895f39c6539f0
DenisLevkovets <levkovets_da@mail.ru>
2018-09-05 22:32:35 +0300

    weeek3

--- week3/ex5.c
+++ week3/ex5.c
@@ -4,6 +4,7 @@
 
 typedef struct Node{
     int value;
+    struct Node *pr;
     struct Node *next;
 } Node;
 
@@ -29,11 +30,13 @@ void insert_node(int value,int position){
     Node* node=(Node*)malloc(sizeof(Node));
     node->value=value;
     if(size==0){
+        node->pr=NULL;
         node->next=NULL;
         head=node;
     }
     else if(position==1){
         node->next=head;
+        head->pr=node;
         head=node;
     }
     else if(position==size+1){
@@ -42,12 +45,16 @@ void insert_node(int value,int position){
     else{
         int pos=1;
         Node *p=head;
-        while(pos!=position-1){
+        while(pos!=position){
             p=p->next;
             pos++;
         }
-        node->next=p->next->next;
-        p->next=node;
+
+
+        node->pr=p->pr;
+        node->next=p;
+        p->pr->next=node;
+        p->pr=node;
     }
     size++;
 }
@@ -56,6 +63,7 @@ void push_node(int value){
     Node* node=(Node*)malloc(sizeof(Node));
     node->value=value;
     if(size==0){
+        node->pr=NULL;
         node->next=NULL;
         head=node;
     }
@@ -65,6 +73,7 @@ void push_node(int value){
             p=p->next;
         }
         p->next=node;
+        node->pr=p;
         node->next=NULL;
     }
 }
@@ -76,10 +85,10 @@ void pop_node(){
     }
     else {
         Node*p=head;
-        while (p->next->next!=NULL){
+        while (p->next!=NULL){
             p=p->next;
         }
-        p->next=NULL;
+        p->pr->next=NULL;
     }
 }
 
@@ -97,16 +106,19 @@ void delete_node(int position){
     else {
         int pos=1;
         Node *p=head;
-        while(pos!=position-1){
+        while(pos!=position){
             p=p->next;
             pos++;
         }
-        p->next=p->next->next;
+        p->pr->next=p->next;
+        p->next->pr=p->pr;
         size--;
     }
 }
 
 int main(){
+
+
     insert_node(3,1);
     insert_node(4,2);
     insert_node(5,3);
@@ -115,4 +127,6 @@ int main(){
     print_list();
     delete_node(size);
     print_list();
+
+
 }
